21a00bf75d9f532bbead355bd29c2ebbb610ce7e
Gleidson <togleid@gmail.com>
2022-05-10 22:47:41 -0300

    refactor: TS-271 Add support for "tag" and "set" type (#13)
    * fix: TS-271 Fix test and add test scripts
    
    * impr: TS-271 Parse AbortIncompleteMultipartUpload
    
    * impr: TS-271 Parse Transitions
    
    * fix: TS-271 Fix break out of loop too soon
    
    * test: TS-271 Ensure default is applied
    
    * impr: TS-271 Abstract mapValueRules and add test
    
    * impr: TS-271 Clean up mapper
    
    * fix: TS-271 Check if resolvedValue type is undefined, as false is a valid value
    
    * impr: TS-271 Add tag type
    
    * impr: TS-271 Consolidate mapper; all tests pass
    
    * impr: TS-271 Further consolidate functions in mapper; remove null values from mapped object
    
    * impr: TS-271 Clean up unnecessary vars
    
    * impr: TS-271 Clean up unnecessary vars
    
    * impr: TS-271 Clean up unnecessary vars

--- src/mapper.js
+++ src/mapper.js
@@ -1,39 +1,104 @@
+/** @format */
+
 const get = require("lodash.get");
 
-function mapper(input, schema) {
-  const mappedObject = {};
-  Object.keys(schema).forEach((schemaKey) => {
-    const dataType = schema[schemaKey].type;
-    const mapItems = schema[schemaKey].mapItems;
-    const defaultValue = schema[schemaKey].defaultValue ?? null;
-    switch (dataType) {
-      case "string":
-      case "number":
-      case "boolean":
-      case "array":
-        if(schema[schemaKey].staticValue) {
-          mappedObject[schemaKey] = schema[schemaKey].staticValue;
-          return;
-        }
-        const resolvedPath = mapItems ? mapItems.find(mapItem => get(input, mapItem) !== undefined) : false;
-        let resolvedValue = resolvedPath ? get(input, resolvedPath) : defaultValue;
-        if(schema[schemaKey].mappingValueRules) {
-          const validMapRule = schema[schemaKey].mappingValueRules.find(rule => rule.original.includes(resolvedValue));
-          if(validMapRule) {
-            resolvedValue = validMapRule.target;
-          }
-        }
-        mappedObject[schemaKey] = resolvedValue;
-        return;
-      case "object":
-        mappedObject[schemaKey] = mapper(
-          input,
-          schema[schemaKey].properties
-        );
-        return;
-      // case "array(objects)":
+const mapValueRules = (mappingValueRules, originalValue) => {
+  let newValue = originalValue;
+  if (mappingValueRules) {
+    const validMapRule = mappingValueRules.find((rule) =>
+      rule.original.includes(originalValue)
+    );
+    if (validMapRule) {
+      newValue = validMapRule.target;
     }
-  });
-  return mappedObject;
-}
+  }
+  return newValue;
+};
+const getMapItems = (input, mapItems) => {
+  let value;
+  if (Array.isArray(mapItems)) {
+    for (const item of mapItems) {
+      value = get(input, item);
+      if (value !== undefined) {
+        break;
+      }
+    }
+  }
+  //return the value for the first mapped item found
+  return value;
+};
+
+const getTag = (mapItems) => {
+  const result = [];
+  for (const Key in mapItems) {
+    const Value = mapItems[Key];
+    result.push({ Key, Value });
+  }
+  if (result.length) {
+    return result;
+  }
+};
+
+const isEmpty = (obj) => {
+  for (const prop in obj) {
+    if (obj.hasOwnProperty(prop)) return false;
+  }
+  return true;
+};
+
+const getSet = (mapItems, properties) => {
+  if (Array.isArray(mapItems)) {
+    let resolvedValues = [];
+    mapItems.forEach((input) => {
+      resolvedValues.push(mapper(input, properties));
+    });
+    return resolvedValues;
+  }
+};
+
+const mapper = (input, schema) => {
+  const mappedObject = {};
+  for (const key in schema) {
+    const value = schema[key];
+    const dataType = value.type;
+    let mapItems;
+    let resolvedValue;
+    let hasMapItems = false;
+
+    if (value.staticValue) {
+      mappedObject[key] = value.staticValue;
+      continue;
+    }
+
+    if (value.mapItems) {
+      mapItems = getMapItems(input, value.mapItems);
+      hasMapItems = typeof mapItems !== "undefined";
+    }
+
+    if (hasMapItems) {
+      if (dataType === "set") {
+        resolvedValue = getSet(mapItems, value.properties);
+      } else if (dataType === "tag") {
+        resolvedValue = getTag(mapItems);
+      } else {
+        resolvedValue = value.properties
+          ? mapper(mapItems, value.properties)
+          : mapValueRules(value.mappingValueRules, mapItems);
+      }
+    } else {
+      resolvedValue = value.properties
+        ? mapper(input, value.properties)
+        : value.defaultValue;
+    }
+
+    if (typeof resolvedValue !== "undefined") {
+      mappedObject[key] = resolvedValue;
+    }
+  }
+
+  if (!isEmpty(mappedObject)) {
+    return mappedObject;
+  }
+};
+
 module.exports = mapper;
--- test/src/mapper.test.js
+++ test/src/mapper.test.js
@@ -1,14 +1,14 @@
+/** @format */
+
 const mapper = require("../../src/mapper");
 const { input1, input2, input3 } = require("../mocks/mapper/data.mock");
-const {bookTemplate} = require("../mocks/mapper/template.mock");
+const { bookTemplate } = require("../mocks/mapper/template.mock");
 describe("mapper", () => {
   describe("When given an input object and a template to match", () => {
     it("Should map the input object the new template keys and return it as an object", () => {
       expect(mapper(input1, bookTemplate)).toMatchObject({
-        OverallRating: null,
-        AdditionalInfo: { Genre: null, Publisher: null },
         Title: "Catcher in the Rye",
-        Author: { Name: "J.D. Salinger", Origin: null },
+        Author: { Name: "J.D. Salinger", Origin: "USA" },
         YearPublished: "1951",
         IsFiction: true,
         NumberOfPages: 234,
@@ -16,11 +16,6 @@ describe("mapper", () => {
     });
     it("Should map the input object to new template keys with the desired data structure", () => {
       expect(mapper(input2, bookTemplate)).toMatchObject({
-        OverallRating: null,
-        AdditionalInfo: {
-          Genre: null,
-          Publisher: null,
-        },
         Title: "Nineteen Eighty-Four",
         Author: { Name: "George Orwell", Origin: "England" },
         YearPublished: "1949",
@@ -40,6 +35,306 @@ describe("mapper", () => {
         YearPublished: 2018,
         IsFiction: false,
         NumberOfPages: 320,
+      });
+    });
+  });
+
+  describe("GIVEN key is mapped to a static value", () => {
+    it("SHOULD add the key to the output, even if it is not in the input", () => {
+      const template = {
+        provider: {
+          type: "string",
+          staticValue: "aws",
+        },
+        configuration: {},
+      };
+      const input = {
+        configuration: {},
+      };
+
+      const result = mapper(input, template);
+      const expected = { provider: "aws" };
+      expect(result).toStrictEqual(expected);
+    });
+  });
+
+  describe("GIVEN mappingValueRules is configured", () => {
+    it("SHOULD replace mappingValueRules.original with .target in the input", () => {
+      const template = {
+        configuration: {
+          type: "object",
+          properties: {
+            ObjectLockConfiguration: {
+              type: "object",
+              properties: {
+                ObjectLockEnabled: {
+                  type: "boolean",
+                  defaultValue: "Disabled",
+                  description: "",
+                  mapItems: [
+                    "configuration.object_lock_enabled",
+                    "configuration.object_lock_configuration[0].object_lock_enabled",
+                  ],
+                  mappingValueRules: [
+                    {
+                      original: ["Enabled", "true", true],
+                      target: "Enabled",
+                    },
+                  ],
+                },
+              },
+            },
+          },
+        },
+      };
+
+      const input = {
+        configuration: { object_lock_enabled: true },
+      };
+
+      const result = mapper(input, template);
+      const expected = {
+        ObjectLockEnabled: "Enabled",
+      };
+      expect(result.configuration.ObjectLockConfiguration).toStrictEqual(
+        expected
+      );
+    });
+  });
+  describe("GIVEN a schema of type 'set'", () => {
+    const template = {
+      configuration: {
+        type: "object",
+        properties: {
+          LifecycleConfiguration: {
+            type: "object",
+            properties: {
+              Rules: {
+                type: "set",
+                mapItems: ["configuration.lifecycle_rule"],
+                properties: {
+                  AbortIncompleteMultipartUpload: {
+                    type: "object",
+                    mapItems: ["abort_incomplete_multipart_upload[0]"],
+                    properties: {
+                      DaysAfterInitiation: {
+                        type: "number",
+                        mapItems: ["days_after_initiation"],
+                      },
+                    },
+                  },
+                  Status: {
+                    type: "string",
+                    mapItems: ["status"],
+                    defaultValue: "Disabled",
+                  },
+                  Transitions: {
+                    type: "set",
+                    mapItems: ["transition"],
+                    properties: {
+                      StorageClass: {
+                        type: "string",
+                        enum: [
+                          "DEEP_ARCHIVE",
+                          "GLACIER",
+                          "GLACIER_IR",
+                          "INTELLIGENT_TIERING",
+                          "ONEZONE_IA",
+                          "STANDARD_IA",
+                        ],
+                        mapItems: ["storage_class"],
+                      },
+                      TransitionDate: {
+                        type: "string",
+                        mapItems: ["date"],
+                      },
+                      TransitionInDays: {
+                        type: "number",
+                        mapItems: ["days"],
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    };
+    describe("AND one lifecycle_rule as input", () => {
+      it("SHOULD add it to Rules", () => {
+        const input = {
+          configuration: {
+            lifecycle_rule: [
+              {
+                abort_incomplete_multipart_upload: [
+                  {
+                    days_after_initiation: 2,
+                  },
+                ],
+              },
+            ],
+          },
+        };
+
+        const result = mapper(input, template);
+
+        const expected = {
+          Rules: [
+            {
+              AbortIncompleteMultipartUpload: {
+                DaysAfterInitiation: 2,
+              },
+              Status: "Disabled",
+            },
+          ],
+        };
+        expect(result.configuration.LifecycleConfiguration).toStrictEqual(
+          expected
+        );
+      });
+    });
+    describe("AND more than one lifecycle_rule as input", () => {
+      it("SHOULD add each to Rules", () => {
+        const input = {
+          configuration: {
+            lifecycle_rule: [
+              {
+                abort_incomplete_multipart_upload: [
+                  {
+                    days_after_initiation: 30,
+                  },
+                ],
+                Status: "Disabled",
+              },
+              {
+                abort_incomplete_multipart_upload: [
+                  {
+                    days_after_initiation: 2,
+                  },
+                ],
+                Status: "Disabled",
+              },
+            ],
+          },
+        };
+
+        const result = mapper(input, template);
+        const expected = {
+          Rules: [
+            {
+              AbortIncompleteMultipartUpload: {
+                DaysAfterInitiation: 30,
+              },
+              Status: "Disabled",
+            },
+            {
+              AbortIncompleteMultipartUpload: {
+                DaysAfterInitiation: 2,
+              },
+              Status: "Disabled",
+            },
+          ],
+        };
+        expect(result.configuration.LifecycleConfiguration).toStrictEqual(
+          expected
+        );
+      });
+    });
+    describe("AND nested sets", () => {
+      it("SHOULD calculate the nested sets", () => {
+        const input = {
+          configuration: {
+            lifecycle_rule: [
+              {
+                id: "GlacierRule",
+                transition: [
+                  {
+                    days: 1,
+                    storage_class: "GLACIER",
+                  },
+                  {
+                    days: 30,
+                    storage_class: "STANDARD_IA",
+                  },
+                ],
+              },
+            ],
+          },
+        };
+
+        const result = mapper(input, template);
+        const expected = {
+          Rules: [
+            {
+              Status: "Disabled",
+              Transitions: [
+                {
+                  StorageClass: "GLACIER",
+                  TransitionInDays: 1,
+                },
+                {
+                  StorageClass: "STANDARD_IA",
+                  TransitionInDays: 30,
+                },
+              ],
+            },
+          ],
+        };
+        expect(result.configuration.LifecycleConfiguration).toStrictEqual(
+          expected
+        );
+      });
+    });
+    describe("AND input is populated and is mapped to a default value", () => {
+      it("SHOULD set the input with the populated value", () => {
+        const input = {
+          configuration: {
+            lifecycle_rule: [
+              {
+                id: "GlacierRule",
+                status: "Enabled",
+              },
+            ],
+          },
+        };
+
+        const result = mapper(input, template);
+        const expected = {
+          Rules: [
+            {
+              Status: "Enabled",
+            },
+          ],
+        };
+        expect(result.configuration.LifecycleConfiguration).toStrictEqual(
+          expected
+        );
+      });
+    });
+    describe("AND input is empty and is mapped to a default value", () => {
+      it("SHOULD set the input with the default value", () => {
+        const input = {
+          configuration: {
+            lifecycle_rule: [
+              {
+                id: "GlacierRule",
+              },
+            ],
+          },
+        };
+
+        const result = mapper(input, template);
+        const expected = {
+          Rules: [
+            {
+              Status: "Disabled",
+            },
+          ],
+        };
+        expect(result.configuration.LifecycleConfiguration).toStrictEqual(
+          expected
+        );
       });
     });
   });
